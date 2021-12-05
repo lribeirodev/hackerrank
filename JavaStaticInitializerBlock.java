cbe924e6535118778f79ae10d77d33b9573eb97d
Lucas Ribeiro <66949329+lribeirodev@users.noreply.github.com>
2021-12-05 13:05:08 -0300

    Java Static Initializer Block

---
+++ JavaStaticInitializerBlock.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+// Solution's start
+static int H, B;
+static boolean flag = false;
+
+static{
+    Scanner s = new Scanner(System.in);
+    B = s.nextInt();
+    H = s.nextInt();
+    
+    try{
+    
+    if(B<=0 || H<=0) throw new Exception();
+    
+    flag = true;
+
+    } catch (Exception e){
+        System.out.println("java.lang.Exception: Breadth and height must be positive");
+    }
+}
+// Solution's End
+
+public static void main(String[] args){
+		if(flag){
+			int area=B*H;
+			System.out.print(area);
+		}
+		
+	}//end of main
+
+}//end of class
